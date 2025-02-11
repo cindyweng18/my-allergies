2c29ffcaa1830fb24935f93d9369ef3d572b4fba
cindyweng18 <cindyweng18@gmail.com>
2025-02-11 15:32:45 -0500

    create form to input allergies..base

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for, session
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello World'
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        allergies = request.form["allergies"].split(",") 
+        allergies = [allergy.strip().lower() for allergy in allergies] 
+
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run()
