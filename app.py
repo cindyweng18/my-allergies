03ba035ac19df14ba9ff3a14ce93999c8d319a47
cindyweng18 <cindyweng18@gmail.com>
2025-02-22 16:00:17 -0500

    minor debugging changes

--- app.py
+++ app.py
@@ -1,29 +1,40 @@
 from flask import Flask
+from flask_migrate import Migrate
 from config import Config
-from extensions import db, mail, serializer
+from extensions import db, mail, bcrypt 
 from flask_login import LoginManager
 from models.database import User
 from routes.auth_routes import auth_bp
 from routes.allergy_routes import allergy_bp
 from routes.password_reset import password_reset
+from itsdangerous import URLSafeTimedSerializer 
 
-app = Flask(__name__)
-app.config.from_object(Config)
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
 
-db.init_app(app)
-mail.init_app(app)
+    db.init_app(app)
+    migrate = Migrate(app, db)
+    mail.init_app(app)
+    bcrypt.init_app(app)
 
-login_manager = LoginManager()
-login_manager.init_app(app)
-login_manager.login_view = "auth.login"
+    global serializer
+    serializer = URLSafeTimedSerializer(app.config["SECRET_KEY"])
 
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
+    login_manager = LoginManager()
+    login_manager.init_app(app)
+    login_manager.login_view = "auth.login"
 
-app.register_blueprint(auth_bp, url_prefix="/auth")
-app.register_blueprint(allergy_bp)
-app.register_blueprint(password_reset)
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
+
+    app.register_blueprint(auth_bp, url_prefix="/auth")
+    app.register_blueprint(allergy_bp)
+    app.register_blueprint(password_reset)
+
+    return app
 
 if __name__ == "__main__":
+    app = create_app()
     app.run(debug=True)
