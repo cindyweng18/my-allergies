a8b0ad39bdbffe5870831045085648d799a09f6d
cindyweng18 <cindyweng18@gmail.com>
2025-04-04 16:40:55 -0400

    side menu of home page ui

---
+++ frontend/src/pages/SideMenu.js
@@ -0,0 +1,112 @@
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Avatar from '@mui/material/Avatar';
+import MuiDrawer, { drawerClasses } from '@mui/material/Drawer';
+import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
+import SettingsRoundedIcon from '@mui/icons-material/SettingsRounded';
+import InfoRoundedIcon from '@mui/icons-material/InfoRounded';
+import HelpRoundedIcon from '@mui/icons-material/HelpRounded';
+import OptionsMenu from './OptionsMenu';
+
+const drawerWidth = 240;
+
+const Drawer = styled(MuiDrawer)({
+  width: drawerWidth,
+  flexShrink: 0,
+  boxSizing: 'border-box',
+  mt: 10,
+  [`& .${drawerClasses.paper}`]: {
+    width: drawerWidth,
+    boxSizing: 'border-box',
+  },
+});
+
+const mainListItems = [
+    { text: 'Home', icon: <HomeRoundedIcon /> },,
+  ];
+  
+  const secondaryListItems = [
+    { text: 'Settings', icon: <SettingsRoundedIcon /> },
+    { text: 'About', icon: <InfoRoundedIcon /> },
+    { text: 'Feedback', icon: <HelpRoundedIcon /> },
+  ];
+
+export default function SideMenu() {
+  return (
+    <Drawer
+      variant="permanent"
+      sx={{
+        display: { xs: 'none', md: 'block' },
+        [`& .${drawerClasses.paper}`]: {
+          backgroundColor: 'background.paper',
+        },
+      }}
+    >
+      <Box
+        sx={{
+          overflow: 'auto',
+          height: '100%',
+          display: 'flex',
+          flexDirection: 'column',
+        }}
+      >
+            <Stack sx={{ flexGrow: 1, p: 1, justifyContent: 'space-between' }}>
+                <List dense>
+                    {mainListItems.map((item, index) => (
+                    <ListItem key={index} disablePadding sx={{ display: 'block' }}>
+                        <ListItemButton selected={index === 0}>
+                        <ListItemIcon>{item.icon}</ListItemIcon>
+                        <ListItemText primary={item.text} />
+                        </ListItemButton>
+                    </ListItem>
+                    ))}
+                </List>
+                <List dense>
+                    {secondaryListItems.map((item, index) => (
+                    <ListItem key={index} disablePadding sx={{ display: 'block' }}>
+                        <ListItemButton>
+                        <ListItemIcon>{item.icon}</ListItemIcon>
+                        <ListItemText primary={item.text} />
+                        </ListItemButton>
+                    </ListItem>
+                    ))}
+                </List>
+                </Stack>
+      </Box>
+      <Stack
+        direction="row"
+        sx={{
+          p: 2,
+          gap: 1,
+          alignItems: 'center',
+          borderTop: '1px solid',
+          borderColor: 'divider',
+        }}
+      >
+        <Avatar
+          sizes="small"
+          alt="Riley Carter"
+          src="/static/images/avatar/7.jpg"
+          sx={{ width: 36, height: 36 }}
+        />
+        <Box sx={{ mr: 'auto' }}>
+          <Typography variant="body2" sx={{ fontWeight: 500, lineHeight: '16px' }}>
+            Riley Carter
+          </Typography>
+          <Typography variant="caption" sx={{ color: 'text.secondary' }}>
+            riley@email.com
+          </Typography>
+        </Box>
+        <OptionsMenu />
+      </Stack>
+    </Drawer>
+  );
+}
