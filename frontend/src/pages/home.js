49dbba998970a9d9c4333809073808a272cb5f7b
cindyweng18 <cindyweng18@gmail.com>
2025-04-06 14:48:49 -0400

    ui changes to home page

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -1,15 +1,169 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import "../App.css"
+import AppTheme from "../theme";
+import { CssBaseline } from "@mui/material";
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+import InputLabel from '@mui/material/InputLabel';
+import Link from '@mui/material/Link';
+import Stack from '@mui/material/Stack';
+import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
+import visuallyHidden from '@mui/utils/visuallyHidden';
+import ColorModeSelect from "../ColorModeSelect";
+import { useNavigate } from "react-router-dom";
 
-function Home() {
+function Home(props) {
+  const navigate = useNavigate();
+  const [email, setEmail] = React.useState('');
+  const [emailError, setEmailError] = React.useState('');
+  const [emailErrorMessage, setEmailErrorMessage] = React.useState('');
+
+  const validateInputs = () => {
+    const email = document.getElementById('email');
+    let isValid = true;
+
+    if (!email.value || !/\S+@\S+\.\S+/.test(email.value)) {
+      setEmailError(true);
+      setEmailErrorMessage('Please enter a valid email address.');
+      isValid = false;
+    } else {
+      setEmailError(false);
+      setEmailErrorMessage('');
+    }
+
+    return isValid;
+  }
+
+  const handleSubmit = () => {
+    if (validateInputs) {
+      localStorage.setItem("email", email);
+      navigate("/register")
+    } else {
+      setEmailError(true);
+      setEmailErrorMessage('Please enter a valid email address.');
+    }
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Allergy Checker</h1>
-        <Link to="/login">Go to Login</Link>
-      </header>
-    </div>
+    <AppTheme {...props}>
+      <CssBaseline enableColorScheme />
+      <ColorModeSelect sx={{ position: 'fixed', top: '1rem', right: '1rem' }} />
+      <Box
+      id="hero"
+      sx={(theme) => ({
+        width: '100%',
+        backgroundRepeat: 'no-repeat',
+        backgroundImage:
+          'radial-gradient(ellipse 80% 50% at 50% -20%, hsl(210, 100%, 90%), transparent)',
+        ...theme.applyStyles('dark', {
+          backgroundImage:
+            'radial-gradient(ellipse 80% 50% at 50% -20%, hsl(210, 100%, 16%), transparent)',
+        }),
+      })}
+    >
+      <Container
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          pt: { xs: 14, sm: 20 },
+          pb: { xs: 8, sm: 12 },
+        }}
+      >
+        <Stack
+          spacing={2}
+          useFlexGap
+          sx={{ alignItems: 'center', width: { xs: '100%', sm: '70%' } }}
+        >
+          <Typography
+            variant="h1"
+            sx={{
+              display: 'flex',
+              flexDirection: { xs: 'column', sm: 'row' },
+              alignItems: 'center',
+              fontSize: 'clamp(3rem, 10vw, 3.5rem)',
+            }}
+          >
+            Allergy&nbsp;
+            <Typography
+              component="span"
+              variant="h1"
+              sx={(theme) => ({
+                fontSize: 'inherit',
+                color: 'primary.main',
+                ...theme.applyStyles('dark', {
+                  color: 'primary.light',
+                }),
+              })}
+            >
+              Checker
+            </Typography>
+          </Typography>
+          <Typography
+            sx={{
+              textAlign: 'center',
+              color: 'text.secondary',
+              width: { sm: '100%', md: '80%' },
+            }}
+          >
+            Discover if any product contain ingredients you are
+            allergic to. Start by inserting your allergies, then 
+            the name of the product, and let us handle the research.
+          </Typography>
+          <Stack
+            direction={{ xs: 'column', sm: 'row' }}
+            spacing={1}
+            useFlexGap
+            sx={{ pt: 2, width: { xs: '100%', sm: '350px' } }}
+          >
+            <InputLabel htmlFor="email-hero" sx={visuallyHidden}>
+              Email
+            </InputLabel>
+            <TextField
+              error={emailError}
+              helperText={emailErrorMessage}
+              id="email-hero"
+              hiddenLabel
+              size="small"
+              variant="outlined"
+              aria-label="Enter your email address"
+              placeholder="Your email address"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              fullWidth
+              slotProps={{
+                htmlInput: {
+                  autoComplete: 'off',
+                  'aria-label': 'Enter your email address',
+                },
+              }}
+            />
+            <Button
+              variant="contained"
+              color="primary"
+              size="small"
+              sx={{ minWidth: 'fit-content' }}
+              onClick={handleSubmit}
+            >
+              Start now
+            </Button>
+          </Stack>
+          <Typography
+            variant="caption"
+            color="text.secondary"
+            sx={{ textAlign: 'center' }}
+          >
+            By clicking &quot;Start now&quot; you agree to our&nbsp;
+            <Link href="#" color="primary">
+              Terms & Conditions
+            </Link>
+            .
+          </Typography>
+        </Stack>
+      </Container>
+    </Box>
+    </AppTheme>
   );
 }
 
