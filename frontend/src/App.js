221ce2542abc6f3e0d1c6c6d0832b1b5cf1e3227
cindyweng18 <cindyweng18@gmail.com>
2025-04-08 06:11:23 -0400

    deleting unneccessary files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
-import Dashboard from "./pages/dashboard";
 import Home from "./pages/home";
 import Register from "./pages/register";
 import AllergyChecker from "./pages/AllergyChecker";
@@ -13,7 +12,6 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/allergy" element={<AllergyChecker />} />
       </Routes>
     </Router>
