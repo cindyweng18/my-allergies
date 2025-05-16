ea5c2ea14ec4403072be52c6138e0dae14d41823
cindyweng18 <cindyweng18@gmail.com>
2025-05-16 19:57:17 -0400

    added edit profile request

--- routes/user.py
+++ routes/user.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from models.database import db, User
 
@@ -17,3 +17,34 @@ def get_profile():
         "username": user.username,
         "email": user.email,
     }), 200
+
+@user_bp.route("/profile", methods=["PUT"])
+@jwt_required()
+def update_profile():
+    user_id = get_jwt_identity()
+    user = User.query.get(user_id)
+
+    if not user:
+        return jsonify({"error": "User not found"}), 404
+
+    data = request.get_json()
+    if not data:
+        return jsonify({"message": "No JSON data received"}), 400
+
+    username = data.get("username", "").strip()
+    email = data.get("email", "").strip()
+
+    if not username or not email:
+        return jsonify({"message": "Username and email are required."}), 400
+
+    conflict = User.query.filter(
+        ((User.email == email) | (User.username == username)) & (User.id != user_id)
+    ).first()
+    if conflict:
+        return jsonify({"message": "Email or Username already taken."}), 400
+
+    user.username = username
+    user.email = email
+    db.session.commit()
+
+    return jsonify({"message": "Profile updated successfully."}), 200
